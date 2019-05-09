3cdb0a9296d2214faec5299f3e2675ef82e48e7c
jacobVillalonga <45976389+jacobVillalonga@users.noreply.github.com>
2019-05-09 23:17:18 +0200

    ...

--- app/routes/index.routes.js
+++ app/routes/index.routes.js
@@ -8,35 +8,16 @@ module.exports = function(app) {
         title: 'Gestor Comunidades'
       })
     });
-    // app.route('/hi')
-    //   .get(viviendaController.list_all_viviendas);
+    app.get('/hi', viviendaController.list_all_viviendas);
 
-        app.get('/hi', function(req, res) {
-          var viviendas = viviendaController.list_all_viviendas;
-          console.log(viviendas.title);
-          console.log("ok");
+      app.get('/hi', function(req, res) {
+        viviendaController.list_all_viviendas;
         	res.sendFile(path.resolve('app/public/index.html'));
-        });
+      });
+        // app.get('/hi', function(req, res) {
+        //   var viviendas = viviendaController.list_all_viviendas;
+        //   console.log(viviendas.title);
+        //   console.log("ok");
+        // 	res.sendFile(path.resolve('app/public/index.html'));
+        // });
 };
-
-// module.exports = function(app) {
-//   app.set('appPath', 'public');
-// app.use(express.static(__dirname +'/public'));
-//
-// app.route('/*')
-//   .get(function(req, res) {
-//     res.sendfile(app.get('appPath') + '/index.html');
-//   });
-//
-//   app.route('/')
-//     .get(function(req, res) {
-//       res.render('index.ejs', {
-//         title: 'Gestor Comunidades'
-//       })
-//     });
-//
-//         app.get('/hi', function(req, res) {
-//           // path, {'root': '/path/to/root/directory'}
-//         	res.sendFile(path.resolve('app/public/index.html'));
-//         });
-// };
